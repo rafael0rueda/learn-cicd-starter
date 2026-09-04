edca564e87c37d662b3c64943957c99d7842f2ad
Rafael Rueda <rrrm85@gmail.com>
2026-09-04 13:57:25 -0500

    changes to test

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORTss")
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
