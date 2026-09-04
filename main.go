516c8819491b2d0c82348bdcec144d2b1f2e839c
Rafael Rueda <rrrm85@gmail.com>
2026-09-04 13:54:06 -0500

    Add test for api key

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
 	}
 
-	port := os.Getenv("PORT")
+	port := os.Getenv("PORTss")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
 	}
