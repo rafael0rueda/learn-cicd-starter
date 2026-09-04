14cc782658589f2d6892cd24741606fa6f29c908
Rafael Rueda <rrrm85@gmail.com>
2026-09-04 13:59:00 -0500

    revert changes in auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -11,7 +11,6 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	authHeader = "NO"
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
